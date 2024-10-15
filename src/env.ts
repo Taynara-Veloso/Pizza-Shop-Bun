82ec900a9e06180fb9a6a49ce9fccdea6dbe4b54
taynara veloso <taynarasantos2.3@hotmail.com>
2024-10-15 20:35:28 -0300

    :star: file envSchema

---
+++ src/env.ts
@@ -0,0 +1,10 @@
+import { z } from 'zod';
+import { config } from 'dotenv';
+
+config({ path: '.env.local' });
+
+const envSchema = z.object({
+  DB_URL: z.string().url().min(1),
+})
+
+export const env = envSchema.parse(process.env);
