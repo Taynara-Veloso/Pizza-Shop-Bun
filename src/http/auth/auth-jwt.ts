b8a433ae81698dc2645ef143c77408066ddea8d8
taynara veloso <taynarasantos2.3@hotmail.com>
2024-10-22 18:32:11 -0300

    :star: Elysia - .derive(passing functions)

--- src/http/auth/auth-jwt.ts
+++ src/http/auth/auth-jwt.ts
@@ -1,14 +1,37 @@
 import jwt from '@elysiajs/jwt'
-import { Elysia, t } from 'elysia'
 import { env } from '../../env'
+import { Elysia, t, type Static } from 'elysia'
 
-export const auth = new Elysia().use(
-  jwt({
-    name: 'jwt',
-    secret: env.JWT_SECRET_KEY,
-    schema: t.Object({
-      sub: t.String(),
-      restaurantId: t.Optional(t.String()),
+const jwtPayload = t.Object({
+  sub: t.String(),
+  restaurantId: t.Optional(t.String()),
+})
+
+export const auth = new Elysia()
+  .use(
+    jwt({
+      name: 'jwt',
+      secret: env.JWT_SECRET_KEY,
+      schema: jwtPayload,
     }),
-  }),
-)
+  )
+  .derive({ as: 'scoped' }, ({ jwt, cookie: { auth } }) => {
+    return {
+      signUserToken: async (payload: Static<typeof jwtPayload>) => {
+        const token = await jwt.sign(payload)
+
+        console.log('Gerando Token JWT', token) // Log para ver o token gerado
+
+        auth.set({
+          value: token,
+          httpOnly: true,
+          maxAge: 60 * 60 * 24 * 7, // 7 Dias
+          path: '/',
+        })
+      },
+
+      signOutDeleteCookie: () => {
+        auth.remove()
+      },
+    }
+  })
