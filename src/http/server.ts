0bf249a76426c32976f7e0afbbe0cd05f5dad795
taynara veloso <taynarasantos2.3@hotmail.com>
2024-10-21 21:58:10 -0300

    :star: server .use(authenticateFromLinks)

--- src/http/server.ts
+++ src/http/server.ts
@@ -1,8 +1,12 @@
 import { Elysia } from 'elysia'
-import { registerRestaurants } from './routes/register-restaurant'
 import { sendAuthLinks } from './routes/send-auth-link'
+import { registerRestaurants } from './routes/register-restaurant'
+import { authenticateFromLinks } from './routes/authenticate-from-link'
 
-const app = new Elysia().use(registerRestaurants).use(sendAuthLinks)
+const app = new Elysia()
+  .use(registerRestaurants)
+  .use(sendAuthLinks)
+  .use(authenticateFromLinks)
 
 app.listen(3333, () => {
   console.log('🧅 Http server running')
