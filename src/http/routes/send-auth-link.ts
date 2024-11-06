2b6148ea554169bc6b562761d0538136cac9d6dd
taynara veloso <taynarasantos2.3@hotmail.com>
2024-11-06 18:55:29 -0300

    :star: nodemailer - link to Authenticate

--- src/http/routes/send-auth-link.ts
+++ src/http/routes/send-auth-link.ts
@@ -1,5 +1,7 @@
 import { env } from '../../env'
 import { Elysia, t } from 'elysia'
+import nodemailer from 'nodemailer'
+import { mail } from '../../lib/mail'
 import { db } from '../../db/connection'
 import { createId } from '@paralleldrive/cuid2'
 import { authLinks } from './../../db/schema/auth-links'
@@ -45,15 +47,23 @@ export const sendAuthLinks = new Elysia()
         code: authLinkCode,
       })
 
-      // Enviar um e-mail
-
       const authLink = new URL('/auth-links/authenticate', env.API_BASE_URL)
 
       authLink.searchParams.set('code', authLinkCode)
       // http://localhost:3333/auth-links/authenticate?code=qzug7mlo9ih3125l382qec0u&redirect=http%3A%2F%2Flocalhost%3A5173
       authLink.searchParams.set('redirect', env.AUTH_REDIRECT_URL)
 
-      console.log(authLink.toString())
+      // Enviar um e-mail
+      const info = await mail.sendMail({
+        from: {
+          name: 'Pizza Shop',
+          address: 'pizza@shop.com',
+        },
+        to: email,
+        subject: 'Authenticate to Pizza Shop',
+        text: `Use the following link to authenticate on Pizza Shop: ${authLink.toString()}`,
+      })
+      console.log(nodemailer.getTestMessageUrl(info))
     },
     {
       body: t.Object({
