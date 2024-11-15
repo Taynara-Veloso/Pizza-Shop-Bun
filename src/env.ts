b0f924662c58e9a2de9c3449ac4796ffec0265cf
taynara veloso <taynarasantos2.3@hotmail.com>
2024-11-15 19:23:32 -0300

    :star: envSchema - resend_api_key

--- src/env.ts
+++ src/env.ts
@@ -8,6 +8,7 @@ const envSchema = z.object({
   AUTH_REDIRECT_URL: z.string().url().min(1),
   DB_URL: z.string().url().min(1),
   JWT_SECRET_KEY: z.string().min(1),
+  RESEND_API_KEY: z.string().min(1),
 })
 
 export const env = envSchema.parse(process.env)
